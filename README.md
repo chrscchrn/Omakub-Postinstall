40307d8cb4951f4df39de6c7f7845d3793c09432
Christopher Cochran <cochranschris@gmail.com>
2024-09-05 23:34:20 -0500

    merging

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Omakub-Postinstall
-Post-install for omakub.org
+
+## Personal Post-install for omakub.org
+
+### Usage
 `eval "$(wget -qO- https://raw.githubusercontent.com/chrscchrn/Omakub-Postinall/main/postinall.sh)"`
 
+### Sets up...
+- Monogame/.NET
 
+
+TODO:
+- C# parser, lsp, extensions found in https://www.lazyvim.org/extras/lang/omnisharp
+- Homebrew
--- postinstall.sh
+++ postinstall.sh
@@ -1,1 +1,16 @@
-echo "test"
+set -e
+
+echo ". . . My Omakub Post-Install . . ."
+
+# brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'export XDG_DATA_DIRS="/home/linuxbrew/.linuxbrew/share:$XDG_DATA_DIRS"' >>~/.profile
+## need fresh install for the others
+
+# dotnet-sdk-8
+sudo apt-get update && sudo apt-get install -y dotnet-sdk-8.0
+
+# Monogame Wine effect completion and Templates
+sudo apt install wget curl p7zip-full wine64
+wget -qO- https://monogame.net/downloads/net8_mgfxc_wine_setup.sh | bash
+dotnet new install MonoGame.Templates.CSharp
